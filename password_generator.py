4a142efac77ae65aa637027d76fcda84875bdddb
CJTAYL <64110892+CJTAYL@users.noreply.github.com>
2022-05-08 06:33:01 -0400

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+# Characters used to generate the password
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+num = string.digits
+symbols = string.punctuation
+
+# Structure of characters
+others = lower + num + symbols
+samp_others = random.sample(others, 17)
+samp_upper = random.sample(upper, 1)
+samp_all = samp_upper + samp_others
+
+# Printing the password
+print("".join(samp_all))
