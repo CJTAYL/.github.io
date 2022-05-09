1db951585637c4e84ceb6c47c0dafafdbe5b0ef1
CJTAYL <64110892+CJTAYL@users.noreply.github.com>
2022-05-09 08:20:40 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,21 @@
 import random
 import string
 
-# Characters used to generate the password
-upper = string.ascii_uppercase
-lower = string.ascii_lowercase
-num = string.digits
-symbols = string.punctuation
+def password_generator():
+  # Characters used to generate the password
+  upper = string.ascii_uppercase
+  lower = string.ascii_lowercase
+  num = string.digits
+  symbols = string.punctuation
 
-# Structure of characters
-others = lower + num + symbols
-samp_others = random.sample(others, 17)
-samp_upper = random.sample(upper, 1)
-samp_all = samp_upper + samp_others
+  # Structure of characters
+  others = lower + num + symbols
+  samp_others = random.sample(others, 17)
+  samp_upper = random.sample(upper, 1)
+  samp_all = samp_upper + samp_others
 
-# Printing the password
-print("".join(samp_all))
+  # Printing the password
+  print("".join(samp_all))
+  
+  password_generator()
+  
